5b1fe24c8ae2d12811e4c4a496f2c617919f7b14
matteo <matteo.deregnaucourt1@gmail.com>
2025-12-03 08:59:52 +0100

    ajt route par defaut

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Authentification />} />
         <Route path="/authentification" element={<Authentification />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
